e366bd2cf0ed6bf51b1cc621d8526cad52684add
Katie Hill <khill.fr.gr@gmail.com>
2025-10-21 14:47:53 +0100

    Hamburger menu added to nav bar

--- src/components/FooterBar/FooterBar.jsx
+++ src/components/FooterBar/FooterBar.jsx
@@ -3,7 +3,7 @@ import './FooterBar.css'
 export default function FooterBar(){
   return (
     <div className="footer-bar">
-      &copy; catseye {new Date().getFullYear()}
+      <p>&copy; Katie Hill {new Date().getFullYear()}</p>
     </div>
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,21 +1,38 @@
 import './NavBar.css'
 import { Link } from 'react-router-dom'
+import { Menu, X } from "lucide-react"
+import { useState } from 'react'
 
 const NavBar = () => {
+    const [menuOpen, setMenuOpen] = useState(false)
+
     return (
         <>
             <nav className="navbar">
                 <div className="left-section">
-                    {/* <div className="logo-image">
-                        <img src={eyeIcon} alt="White silhouette of eye" />
-                    </div> */}
                     <Link to="/" className="home-link">KHILLFRGR</Link>
                 </div>
 
-                <div className="center-section">
+                <div className="right-section">
                     <Link to="/" className="profile-button">Projects</Link>
                     <Link to="/" className="profile-button">About</Link>
                 </div>
+                <div className="mobile-user-controls">
+                    <button
+                        className="hamburger"
+                        onClick={() => setMenuOpen(prev => !prev)}
+                    >
+                        {menuOpen ? <X size={24} color="var(--light-fonts)" /> : <Menu size={24} color="var(--light-fonts)" />}
+                    </button>
+                </div>
+                {menuOpen && (
+                    <div className="mobile-menu">
+                        <div className="mobile-links">
+                            <Link to="/" className="mobile-link" onClick={() => setMenuOpen(false)}>Projects</Link>
+                            <Link to="/" className="mobile-link" onClick={() => setMenuOpen(false)}>About</Link>
+                        </div>
+                    </div>
+                )}
             </nav>
         </>
     )
