53c1ff5b7944bea012004ac8e210c97b08e727da
Katie Hill <khill.fr.gr@gmail.com>
2025-10-21 13:02:24 +0100

    Styling added

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
   return (
     <main>
       <section className="hero">
-        <h1>Collaborate, Translate, Manage WorkFlows</h1>
+        <h1>Full-stack developer, React, Node.js, Python</h1>
         <p>Keep all your multilingual content in one place.</p>
         <Link to="/projects" className="page-button">Get started</Link>
       </section>
