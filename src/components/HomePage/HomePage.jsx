118bf4599cae5854306756ada89f35d3796a6b63
Katie Hill <khill.fr.gr@gmail.com>
2025-10-21 16:59:34 +0100

    Projects axios calls added to services & projects added to homepage (need to fix CORS)

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,15 +1,57 @@
 import './HomePage.css'
+import { useState, useEffect, useContext } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
+import { projectsIndex } from '../../services/projects.js'
 
 const HomePage = () => {
   const navigate = useNavigate()
+  const [projects, setProjects] = useState([])
+  const [projectsLoading, setProjectsLoading] = useState(true)
+
+  useEffect(() => {
+    const loadProjects = async () => {
+      try {
+        setProjectsLoading(true)
+        const projects = await projectsIndex()
+        setProjects(projects.data)
+      } catch (error) {
+        console.error('Error:', error)
+        setProjects([])
+      } finally {
+        setProjectsLoading(false)
+      }
+    }
+    loadProjects()
+  }, [])
 
   return (
     <main>
       <section className="hero">
         <h1>Full-stack developer, React, Node.js, Python</h1>
-        <p>Keep all your multilingual content in one place.</p>
-        <Link to="/projects" className="page-button">Get started</Link>
+      </section>
+
+      <section className="page-content">
+        {projectsLoading ? (
+          <p>Loading projects...</p>
+        ) : projects.length > 0 ? (
+          projects.map((project) => (
+              <div key={project.id} className="project-row">
+                <div className="project-info">
+                  <h3>{project.name}</h3>
+                  <div className="project-description">
+                    <p>{project.description}</p>
+                  </div>
+                </div>
+                {/* <div className="project-actions">
+                  <Link to={`/projects/${project.id}`} className="project-button">
+                    View project
+                  </Link>
+                </div> */}
+              </div>
+            ))
+          ) : (
+            <p> No projects found</p>
+          )}
       </section>
     </main>
   )
---
+++ src/services/projects.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+const BASE_URL = import.meta.env.VITE_API_URL
+
+export const projectsIndex = () => {
+    return axios.get(`${BASE_URL}/projects`)
+}
+
+export const projectShow = (projectId) => {
+    return axios.get(`${BASE_URL}/projects/${projectId}/`)
+}
+
+export const projectCreate = (formData) => {
+    return axios.post(`${BASE_URL}/projects/`, formData, {
+        headers: {
+            Authorization: `Bearer ${getToken()}`
+        }
+    })
+}
+
+export const projectUpdate = (projectId, formData) => {
+    const token = getToken()
+    return axios.put(`${BASE_URL}/projects/${projectId}/`, formData, {
+        headers: {
+            Authorization: `Bearer ${getToken()}`
+        }
+    })
+}
+
+export const projectDelete = (projectId) => {
+    return axios.delete(`${BASE_URL}/projects/${projectId}/`, {
+        headers: {
+            Authorization: `Bearer ${getToken()}`
+        }
+    })
+}
