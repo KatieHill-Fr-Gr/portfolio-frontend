22a7c6b41bccfb51b95a5309969170063aa9bf0f
Katie Hill <khill.fr.gr@gmail.com>
2025-11-04 10:30:37 +0000

    Custom useCarouselButtons hook added

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,4 +1,5 @@
 import './Carousel.css'
+import useEmblaCarousel from 'embla-carousel-react'
 
 // import { DotButton, useDotButton } from './EmblaCarouselDotButton'
 // import {
@@ -6,34 +7,39 @@ import './Carousel.css'
 //   NextButton,
 //   usePrevNextButtons
 // } from './EmblaCarouselArrowButtons'
-// import useEmblaCarousel from 'embla-carousel-react'
+
 
 export const Carousel = ({ items, type = "project" }) => {
-    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true, align: "start"})
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true, align: "start" })
 
-    const { prevBtnDisabled, nextBtnDisabled, onPrevButtonClick, onNextButtonClick } =
+  const { prevBtnDisabled, nextBtnDisabled, onPrevButtonClick, onNextButtonClick } =
     usePrevNextButtons(emblaApi)
-    const { selectedIndex, scrollSnaps, onDotButtonClick } = useDotButton(emblaApi)
+  const { selectedIndex, scrollSnaps, onDotButtonClick } = useDotButton(emblaApi)
 
-    return (
+  useEffect(() => {
+    if (emblaApi) {
+      console.log(emblaApi.slideNodes())
+    }
+  }, [emblaApi])
+
+  return (
     <div className="carousel">
       <div className="carousel__viewport" ref={emblaRef}>
         {items.length > 0 ? (
           <div className="carousel__container">
             {items.map((item) => {
-                <div key={item._id} className="carousel__slide">
-                  <div className="item-image">
-                    <img src={item.images?.[0]} alt={item.name} />
-                  </div>
-                  <div className="item-info">
-                      <h3>{item.name}</h3>
-                      <span>{item.subtitle}</span>
-                    </div>
-                    <p className="item-description">{item.description}</p>
-                    <Link to={`/${type}/${item._id}`} className="project-link">
-                      View
-                    </Link>
+              <div key={item._id} className="carousel__slide">
+                <div className="item-image">
+                  <img src={item.images?.[0]} alt={item.name} />
                 </div>
+                <div className="item-info">
+                  <h3>{item.name}</h3>
+                  <span>{item.subtitle}</span>
+                </div>
+                <Link to={`/${type}/${item._id}`} className="project-link">
+                  View
+                </Link>
+              </div>
             })}
           </div>
         ) : (
@@ -51,12 +57,11 @@ export const Carousel = ({ items, type = "project" }) => {
           <DotButton
             key={index}
             onClick={() => onDotButtonClick(index)}
-            className={`carousel__dot ${
-              index === selectedIndex ? "is-selected" : ""
-            }`}
+            className={`carousel__dot ${index === selectedIndex ? "is-selected" : ""
+              }`}
           />
         ))}
       </div>
-      </div>
+    </div>
   )
 } 
---
+++ src/hooks/useCarouselButtons.js
@@ -0,0 +1,28 @@
+import { useCallback, useEffect, useState } from 'react'
+
+export const useCarouselButtons = (emblaApi) => {
+    const [prevBtnDisabled, setPrevBtnDisabled] = useState(true)
+    const [nextBtnDisabled, setNextBtnDisabled] = useState(true)
+
+    const scrollPrev = useCallback(() => {
+        if (emblaApi) emblaApi.scrollPrev()
+    }, [emblaApi]);
+
+    const scrollNext = useCallback(() => {
+        if (emblaApi) emblaApi.scrollNext()
+    }, [emblaApi])
+
+    const onSelect = useCallback((emblaApi) => {
+        setPrevBtnDisabled(!emblaApi.canScrollPrev())
+        setNextBtnDisabled(!emblaApi.canScrollNext())
+    }, [])
+
+    useEffect(() => {
+        if (!emblaApi) return
+
+        onSelect(emblaApi)
+        emblaApi.on('reInit', onSelect).on('select', onSelect)
+    }, [emblaApi, onSelect])
+
+    return { scrollPrev, scrollNext, prevBtnDisabled, nextBtnDisabled }
+}
